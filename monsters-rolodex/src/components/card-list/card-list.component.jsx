40ef92f7604e4d499e6a57c4931e719ef68328e3
Soobratty Anas <soobratty.as@gmail.com>
2022-07-14 15:08:19 +0400

    seperating divs

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./card-list.style.css"
+import { Card } from "../card/card.component";
 
-export const CardList = (props) =>{ 
-    console.log(props);
+export const CardList = (props) =>{
     return (
-        <div className="card-list">{props.children}</div>
+        <div className="card-list">
+          {props.monsters.map(monster =>
+            <Card  key={monster.id} monster = {monster}/>
+            )}
+        </div>
     )
 }
